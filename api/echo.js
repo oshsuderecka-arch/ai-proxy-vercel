853cf294226c37b82451da08b4c1f513de50e070
oshsuderecka-arch <oshsuderecka@gmail.com>
2025-10-17 10:03:49 +0600

    Update echo.js

--- api/echo.js
+++ api/echo.js
@@ -1,1 +1,9 @@
+export default async function handler(req, res) {
+  let body = "";
+  for await (const chunk of req) body += chunk;
+  res.status(200).json({
+    method: req.method,
+    body: body ? JSON.parse(body) : null
+  });
+}
 
